0a8dd62e87c6e3810e031d43a7b058ac384be755
Anshid <muhammedanshidh.t@gmail.com>
2024-08-13 16:22:11 +0530

    12

--- src/MainRoute.jsx
+++ src/MainRoute.jsx
@@ -1,12 +1,10 @@
-import { Route, Routes } from "react-router-dom";
+import { Router, Route, Routes } from "react-router-dom";
 import AdminLogin from "../src/components/admin.components/AdminLogin";
 import AdminSidebar from "./components/admin.components/AdminSidebar";
 import AdminHome from "./pages/admin.pages/AdminHome";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
-import Assemble from "./components/admin.components/Assemble";
-import StatusCards from "./components/admin.components/StatusCards";
 
 export const Axios = axios.create({
     baseURL: "http://localhost:3333/api"
@@ -15,15 +13,17 @@ export const Axios = axios.create({
 
 const MainRoute = () => {
   return (
+    // <Router>
     <>
       <ToastContainer />
       <Routes>
         <Route path="/admin-login" element={<AdminLogin />} />
 
         <Route path="/admin" element={<AdminSidebar />}>
-          <Route index element={<StatusCards/>} />
+          <Route path="home" element={<AdminHome />} />
         </Route>
       </Routes>
+      {/* // </Router> */}
     </>
   );
 };
